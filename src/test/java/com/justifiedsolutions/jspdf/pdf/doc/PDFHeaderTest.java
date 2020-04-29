dd188a21a16e05d3a225ee9b08aec406a89e483f
Jay Burgess <jragingfury@gmail.com>
2020-04-28 20:11:00 -0600

    Add unit test for PDFHeader

---
+++ src/test/java/com/justifiedsolutions/jspdf/pdf/doc/PDFHeaderTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Justified Solutions
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.justifiedsolutions.jspdf.pdf.doc;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class PDFHeaderTest {
+
+    @Test
+    void writeToPDF() throws IOException {
+        ByteArrayOutputStream expected = new ByteArrayOutputStream();
+        expected.writeBytes("%PDF-1.7\n%\u00E2\u00E3\u00CF\u00D3\n".getBytes(StandardCharsets.ISO_8859_1));
+
+        ByteArrayOutputStream actual = new ByteArrayOutputStream();
+        PDFHeader header = new PDFHeader();
+        header.writeToPDF(actual);
+        assertArrayEquals(expected.toByteArray(), actual.toByteArray());
+    }
+}
