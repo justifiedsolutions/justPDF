2446185d3c8ae47b74f5e9f68e18a84910082eca
Jay Burgess <jragingfury@gmail.com>
2020-04-29 16:52:35 -0600

    Create new PDFWritable interface and extract writeToPDF method to it

---
+++ src/main/java/com/justifiedsolutions/jspdf/pdf/PDFWritable.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Justified Solutions
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.justifiedsolutions.jspdf.pdf;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * An interface for writing an object to a PDF document.
+ */
+public interface PDFWritable {
+
+    /**
+     * Allows an object to write itself to a PDF document. This method will take into account any special formatting,
+     * escaping, delimiting, or conversion to the object that must occur to represent it in the document.
+     *
+     * @param pdf an {@link OutputStream} that represents the document or part of the document
+     * @throws IOException if there is an issue writing the object to the OutputStream
+     */
+    void writeToPDF(OutputStream pdf) throws IOException;
+}
--- src/main/java/com/justifiedsolutions/jspdf/pdf/object/PDFObject.java
+++ src/main/java/com/justifiedsolutions/jspdf/pdf/object/PDFObject.java
@@ -5,21 +5,12 @@
 
 package com.justifiedsolutions.jspdf.pdf.object;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import com.justifiedsolutions.jspdf.pdf.PDFWritable;
 
 /**
  * An interface that represents all <code>object</code> types in a PDF document.
  *
  * @see "ISO 32000-1:2008, 7.3"
  */
-public interface PDFObject {
-    /**
-     * Allows a PDFObject to write itself to a PDF document. This method will take into account any special formatting,
-     * escaping, delimiting, or conversion to the object that must occur to represent it in the document.
-     *
-     * @param pdf an {@link OutputStream} that represents the document or part of the document
-     * @throws IOException if there is an issue writing the object to the OutputStream
-     */
-    void writeToPDF(OutputStream pdf) throws IOException;
+public interface PDFObject extends PDFWritable {
 }
