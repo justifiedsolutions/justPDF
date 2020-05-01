d86a7c4d766625f9e08db3f571d9cb032b131291
Jay Burgess <jragingfury@gmail.com>
2020-05-01 12:44:25 -0600

    Add path painting operators

---
+++ src/main/java/com/justifiedsolutions/jspdf/pdf/contents/StrokePath.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 Justified Solutions
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.justifiedsolutions.jspdf.pdf.contents;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Implements the PDF command <code>S</code> to stroke a path in a content stream.
+ *
+ * @see "ISO 32000-1:2008, 8.5.3.1"
+ * @see "ISO 32000-1:2008, 8.5.3.2"
+ */
+public class StrokePath implements PathPaintingGraphicsOperator {
+    @Override
+    public void writeToPDF(OutputStream pdf) throws IOException {
+        pdf.write('S');
+        pdf.write('\n');
+    }
+}
---
+++ src/test/java/com/justifiedsolutions/jspdf/pdf/contents/StrokePathTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020 Justified Solutions
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.justifiedsolutions.jspdf.pdf.contents;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class StrokePathTest {
+
+    @Test
+    void writeToPDF() throws IOException {
+        byte[] expected = {(byte) 'S', (byte) '\n'};
+        ByteArrayOutputStream actual = new ByteArrayOutputStream();
+        new StrokePath().writeToPDF(actual);
+        assertArrayEquals(expected, actual.toByteArray());
+    }
+}
