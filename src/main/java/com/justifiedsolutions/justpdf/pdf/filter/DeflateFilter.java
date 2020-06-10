05932ad3e0389c4317efb22668c12f9c0482f427
Jay Burgess <jragingfury@gmail.com>
2020-06-10 14:15:15 -0600

    Add copyright notice to files that were missing one

--- src/main/java/com/justifiedsolutions/justpdf/pdf/filter/DeflateFilter.java
+++ src/main/java/com/justifiedsolutions/justpdf/pdf/filter/DeflateFilter.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 Justified Solutions
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.justifiedsolutions.justpdf.pdf.filter;
 
 import com.justifiedsolutions.justpdf.pdf.object.PDFDictionary;
